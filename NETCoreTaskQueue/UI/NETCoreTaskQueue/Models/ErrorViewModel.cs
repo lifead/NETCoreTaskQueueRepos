eab9c1ef254a385a0d3bf615831bb4fba0b3c84c
Евгений <vasilev.jv@gmail.com>
2020-05-03 23:24:28 +0700

    Добавлен пустой проект UI

---
+++ NETCoreTaskQueue/UI/NETCoreTaskQueue/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NETCoreTaskQueue.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
